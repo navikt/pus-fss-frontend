0baae6726ff747a8e9d24cbb8be345a407b95f2d
alexander-svendsen <alexander.svendsen@nav.no>
2018-07-12 15:32:52 +0200

    PUS-14: Må ha servlet for SPA funksjonalitet

--- src/main/java/no/nav/fssfrontend/ApplicationConfig.java
+++ src/main/java/no/nav/fssfrontend/ApplicationConfig.java
@@ -5,6 +5,9 @@ import no.nav.apiapp.config.ApiAppConfigurator;
 import no.nav.sbl.util.EnvironmentUtils;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.ServletContext;
+
+import static no.nav.apiapp.ServletUtil.leggTilServlet;
 import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
@@ -19,6 +22,11 @@ public class ApplicationConfig implements ApiApplication.NaisApiApplication {
     @Override
     public String getContextPath() {
         return EnvironmentUtils.requireApplicationName();
+    }
+
+    @Override
+    public void startup(ServletContext servletContext) {
+        leggTilServlet(servletContext, ApplicationServlet.class, "/*");
     }
 
     @Override
