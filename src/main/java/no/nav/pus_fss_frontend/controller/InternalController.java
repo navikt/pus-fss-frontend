71f22111c59ff3c1fa8307f3a76d79e0bced4ccc
Alexander Gård <alex.gaard@hotmail.com>
2020-08-28 13:10:31 +0200

    FO-3002 La til endepunkt og metrikker for selftest

--- src/main/java/no/nav/pus_fss_frontend/controller/InternalController.java
+++ src/main/java/no/nav/pus_fss_frontend/controller/InternalController.java
@@ -1,17 +1,45 @@
 package no.nav.pus_fss_frontend.controller;
 
+import no.nav.common.health.selftest.SelfTestChecks;
+import no.nav.common.health.selftest.SelfTestUtils;
+import no.nav.common.health.selftest.SelftTestCheckResult;
+import no.nav.common.health.selftest.SelftestHtmlGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/internal")
 public class InternalController {
+
+    private final SelfTestChecks selfTestChecks;
+
+    @Autowired
+    public InternalController(SelfTestChecks selfTestChecks) {
+        this.selfTestChecks = selfTestChecks;
+    }
 
     @GetMapping("/isAlive")
     public void isAlive() {}
 
     @GetMapping("/isReady")
     public void isReady() {}
+
+    @GetMapping("/selftest")
+    public ResponseEntity selftest() {
+        List<SelftTestCheckResult> checkResults = SelfTestUtils.checkAll(selfTestChecks.getSelfTestChecks());
+        String html = SelftestHtmlGenerator.generate(checkResults);
+        int status = SelfTestUtils.findHttpStatusCode(checkResults, true);
+
+        return ResponseEntity
+                .status(status)
+                .contentType(MediaType.TEXT_HTML)
+                .body(html);
+    }
 
 }
--- src/test/java/no/nav/pus_fss_frontend/config/TestApplicationConfig.java
+++ src/test/java/no/nav/pus_fss_frontend/config/TestApplicationConfig.java
@@ -1,18 +1,14 @@
 package no.nav.pus_fss_frontend.config;
 
-import no.nav.common.featuretoggle.UnleashService;
-import no.nav.common.featuretoggle.UnleashServiceConfig;
-import no.nav.pus_fss_frontend.config.EnvironmentProperties;
+import no.nav.common.health.selftest.SelfTestChecks;
+import no.nav.common.health.selftest.SelfTestMeterBinder;
 import no.nav.pus_fss_frontend.controller.InternalController;
-import no.nav.pus_fss_frontend.servlet.LoginServlet;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import static java.util.Optional.ofNullable;
-import static no.nav.common.utils.EnvironmentUtils.requireApplicationName;
+import java.util.Collections;
 
 @Configuration
 @EnableConfigurationProperties({EnvironmentProperties.class})
@@ -35,5 +31,16 @@ public class TestApplicationConfig {
 //                .build()
 //        );
 //    }
+
+    @Bean
+    public SelfTestChecks selfTestChecks() {
+        // Vi har ingen avhengigheter som vi trenger å sjekke
+        return new SelfTestChecks(Collections.emptyList());
+    }
+
+    @Bean
+    public SelfTestMeterBinder selfTestMeterBinder(SelfTestChecks selfTestChecks) {
+        return new SelfTestMeterBinder(selfTestChecks);
+    }
 
 }
